bcbd83d50e39f3ca1d2079a71d72d80c25af7a04
rasikapohankar <rasikapohankar@gmail.com>
2017-03-01 00:55:44 +0530

    Write test using mocha and node assert

--- test.js
+++ test.js
@@ -1,42 +1,19 @@
-var assert = require('assert'),
-    http  = require('http');
-
-
-// function test(url) {
-//     var result;
-
-//     http.get(url, function(response) {
-//         response.setEncoding('utf8');
-        
-//         response.on('data', function(data) {
-//             console.log("data = " + data);
-//             result = data;
-//             return result;
-//         });
-        
-//         response.on('error', function(error) {
-//             console.log('error');
-//             return result;
-//         });
-//     }).on('error', function(error) {
-//         console.log('error');
-//         result = error;
-//     });
-// }
-
+var assert  = require('assert'),
+    http    = require('http'),
+    request = require('request'),
+    expect  = require('chai').expect;
 
 describe('timestamp microservice', function() {
-    it('should return the unix and date format', function() {
-        superagent
-        .get('http://localhost:8081/timestamp/1st%20Jan%202017')
-        .end(function(response) {
-            console.log(response);            
+    it('should return the unix and date format', function(done) {
+
+        http.get('http://localhost:8081/timestamp/1st%20Jan%202017', function(response) {
+            response.setEncoding('utf8');
+            response.on('data', function(data) {
+                assert.deepStrictEqual(JSON.parse(data), {"unix":1483209000000,"date":"2016-12-31T18:30:00.000Z"}, "test seems to have failed");
+                done();
+            });
+        }).on('error', function(error) {
+            console.log('error');
         });
     });
 });
-
-//console.log("return value == " + test('http://localhost:8081/timestamp/1st%20Jan%202017'));
-
-//assert.deepStrictEqual(test('http://localhost:8081/timestamp/-4'), {unix:null, date: null}, 'message');
-//assert.deepStrictEqual(test('http://localhost:8081/timestamp/1st%20Jan%202017'), {"unix":1483209000000,"date":"2016-12-31T18:30:00.000Z"}, 'message');
-//assert.deepStrictEqual(test('http://localhost:8081/timestamp/v'), {unix:null, date: null}, 'message');
