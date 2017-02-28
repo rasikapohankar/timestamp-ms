e32e92dc262cd143ceb7a905672b557872d6a928
rasikapohankar <rasikapohankar@gmail.com>
2017-03-01 00:58:06 +0530

    Modify test to use request module, and expect assertion

--- test.js
+++ test.js
@@ -1,19 +1,17 @@
 var assert  = require('assert'),
-    http    = require('http'),
     request = require('request'),
     expect  = require('chai').expect;
+
 
 describe('timestamp microservice', function() {
     it('should return the unix and date format', function(done) {
 
-        http.get('http://localhost:8081/timestamp/1st%20Jan%202017', function(response) {
-            response.setEncoding('utf8');
-            response.on('data', function(data) {
-                assert.deepStrictEqual(JSON.parse(data), {"unix":1483209000000,"date":"2016-12-31T18:30:00.000Z"}, "test seems to have failed");
-                done();
-            });
-        }).on('error', function(error) {
-            console.log('error');
+        var url = 'http://localhost:8081/timestamp/1st%20Jan%202017';
+
+        request(url, function(error, response, body) {
+            var parsedBody = JSON.parse(body);
+            expect(parsedBody).to.deep.equal({"unix":1483209000000,"date":"2016-12-31T18:30:00.000Z"});
+            done();
         });
     });
 });
